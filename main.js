c3586d6a5e7b8a075cb0c2be92744f7336788748
Jules LE BRIS <jls.lebris@gmail.com>
2019-02-16 16:26:50 +0100

    creatematrix

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const fs = require('fs');
+
 function createMatrix(file) {
-    f = new File(file);
+    let contenu = fs.readFileSync(file,'utf8');
+    let firstLine = contenu.split("\n")[0].split(" ");
+    let x = firstLine[0];
+    let y = firstLine[1];
+    let matrix = [];
+    let line = [];
+    for (let index = 0; index < y; index++) {
+        line.push(true);
+    }
+    for (let index = 0; index < x; index++) {
+        matrix.push(line);
+        
+    }
+    return matrix;
 }
+
+console.log(createMatrix("data/a_example.in"))
