e65240ddc41813beb6d4da2591f44a1fa75fe818
Jules LE BRIS <jls.lebris@gmail.com>
2019-02-16 18:18:32 +0100

    start checkSliceBoundaries

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const Coordinate = require('./Coordinate');
 
-const fileName = process.argv.slice(2)[0]
-console.log(fileName)
+const fileName = process.argv.slice(2)[0];
 
 function createMatrixBool() {
     param = getParam(fileName)
-    let r = param.r;
-    let c = param.c;
+    let row = param.row;
+    let col = param.col;
     let matrix = [];
     let line = [];
-    for (let index = 0; index < c; index++) {
+    for (let index = 0; index < col; index++) {
         line.push(true);
     }
-    for (let index = 0; index < r; index++) {
+    for (let index = 0; index < row; index++) {
         matrix.push(line);
-        
     }
     return matrix;
 }
@@ -24,15 +22,18 @@ function getParam(){
     let contenu = fs.readFileSync(fileName,'utf8');
     firstline = contenu.split("\n")[0].split(" ");
     return {
-        r:firstline[0],
-        c:firstline[1],
+        row:firstline[0],
+        col:firstline[1],
         nbIngredient:firstline[2],
         sizeMax:firstline[3]
-    }
+    };
 }
 
 function checkSliceBoundaries(coordinate){
-    
+    if(coordinate.c1.row<getParam().r && coordinate.c1.col<getParam().c &&
+        coordinate.c2.row<getParam().r && coordinate.c2.col<getParam().c){
+        return coordinate;
+    }
 }
 
 console.log(createMatrixBool())
