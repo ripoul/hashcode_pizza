5fef97d2cf8ff0fac116032b03a6ea9bdb81d0fd
Antoine <antoinedrouard@gmail.com>
2019-02-16 17:58:37 +0100

    .

--- Coordinate.js
+++ Coordinate.js
@@ -1,8 +1,6 @@
 class Coordinate{
-    constructor(x1,y1,x2,y2){
-        this.x1=x1;
-        this.y1=y1;
-        this.x2=x2;
-        this.y2=y2;
+    constructor(col,row){
+        this.col=col;
+        this.row=row;
     }
 }
--- main.js
+++ main.js
@@ -4,16 +4,15 @@ const Coordinate = require('./Coordinate');
 function createMatrixBool(file) {
     let contenu = fs.readFileSync(file,'utf8');
     let firstLine = contenu.split("\n")[0].split(" ");
-    let x = firstLine[0];
-    let y = firstLine[1];
+    let row = firstLine[0];
+    let col = firstLine[1];
     let matrix = [];
     let line = [];
-    for (let index = 0; index < y; index++) {
+    for (let index = 0; index < col; index++) {
         line.push(true);
     }
-    for (let index = 0; index < x; index++) {
+    for (let index = 0; index < row; index++) {
         matrix.push(line);
-        
     }
     return matrix;
 }
@@ -21,4 +20,17 @@ function createMatrixBool(file) {
 function checkSliceBoundaries(coordinate){
 }
 
+function getStartCoord(){
+	
+	point= new Coordinate(0, 0);
+	while(point.isTaken()) {
+		if(point.getCol()!=point.getMaxCol()){
+			point.setCol(point.getCol()+1);
+		} else {
+			point.setRow(point.getRow()+1)
+			point.setCol(0);
+		}
+	}
+	return point;
+}
 console.log(createMatrixBool("data/a_example.in"))
